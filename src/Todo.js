80e69442e8e5714eb54e37a4966cc0123abdca99
Koty Stannard <k.stannard@live.com>
2024-05-10 13:32:48 -0500

    Add static priority options to Todo class

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,19 @@
 export class Todo {
+
+    static PRIORITY = ["low", "medium", "high", "very high"]
+
     constructor(title, description, dueDate, priority) {
         this.title = title
         this.description = description
         this.dueDate = dueDate
-        this.priority = priority
+        this.setPriority(priority)
+    }
+
+    setPriority(priority) {
+        if(Todo.PRIORITY.includes(priority)) {
+            this.priority = priority
+        } else {
+            throw new Error(`Invalid priority type. Types are: ${Todo.PRIORITY.join(', ')}`)
+        }
     }
 }
--- src/index.js
+++ src/index.js
@@ -7,10 +7,10 @@ tasks.addTodo('Create website', 'write up the code for the site', 'may 1 2024', 
 tasks.addTodo('clean room', 'vacuum and throw away unused clothes', 'june 1 2024', 'medium')
 
 const task2 = new Project("project a")
-task2.addTodo('a','a','a','a')
+task2.addTodo('a','a','a','low')
 
 const task3 = new Project("project b")
-task3.addTodo('b','b','b','b')
+task3.addTodo('b','b','b','very high')
 
 projects.addProject(tasks)
 projects.addProject(task2)
